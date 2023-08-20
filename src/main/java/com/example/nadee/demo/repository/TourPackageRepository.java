77aae270ac92ab082e88e62ffbfdad28074bd7da
Nadeeshika senavirathna <nadeesenavirathna@gmail.com>
2023-08-20 12:26:02 +0200

    SPRIN-195 - Implement Spring dat query methods

--- src/main/java/com/example/nadee/demo/repository/TourPackageRepository.java
+++ src/main/java/com/example/nadee/demo/repository/TourPackageRepository.java
@@ -3,5 +3,14 @@ package com.example.nadee.demo.repository;
 import com.example.nadee.demo.domain.TourPackage;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface TourPackageRepository extends CrudRepository<TourPackage, String> {
+
+    /**
+     * Spring query method - Find Tour Package by name
+     * @param name - name of the package
+     * @return optional of TourPackage
+     */
+    Optional<TourPackage> findByName(String name);
 }
--- src/main/java/com/example/nadee/demo/repository/TourRepository.java
+++ src/main/java/com/example/nadee/demo/repository/TourRepository.java
@@ -1,7 +1,13 @@
 package com.example.nadee.demo.repository;
 
 import com.example.nadee.demo.domain.Tour;
+import com.example.nadee.demo.types.Difficulty;
+import com.example.nadee.demo.types.Region;
 import org.springframework.data.repository.CrudRepository;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Optional;
 
 /**
  * TourRepository interface
@@ -9,4 +15,26 @@ import org.springframework.data.repository.CrudRepository;
  * Integer - ID type (ID of the entity)
  */
 public interface TourRepository extends CrudRepository<Tour, Integer> {
+
+    List<Tour> findByTourPackageCode(String code);
+
+    /**
+     * Return one Tour, because title is unique
+     */
+    Optional<Tour> findByTitle(String title);
+
+    List<Tour> findByPrice(Integer price);
+
+    /**
+     * Invalid - throws IncorrectResultSizeDataAccessException
+     * @param difficulty
+     * @return
+     */
+    Collection<Tour> findByDifficulty(Difficulty difficulty);
+
+    List<Tour> findByRegion(Region region);
+
+    List<Tour> findByTourPackageCodeAndRegion(String code, Region region);
+
+    List<Tour> findByRegionIn(List<Region> regions);
 }

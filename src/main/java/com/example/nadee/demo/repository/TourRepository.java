9278a8c7faae3a62c6c6c7a295e947750dcc3c9a
Nadeeshika senavirathna <nadeesenavirathna@gmail.com>
2023-08-21 20:46:52 +0200

    SPRIN-200 - controlling API exposure with DELETE PUT and PATCH APIs

--- src/main/java/com/example/nadee/demo/repository/TourRepository.java
+++ src/main/java/com/example/nadee/demo/repository/TourRepository.java
@@ -6,8 +6,8 @@ import com.example.nadee.demo.types.Region;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 import java.util.Collection;
 import java.util.List;
@@ -18,8 +18,8 @@ import java.util.Optional;
  * Tour - domain type
  * Integer - ID type (ID of the entity)
  */
-//public interface TourRepository extends CrudRepository<Tour, Integer> {
-public interface TourRepository extends PagingAndSortingRepository<Tour, Integer> {
+public interface TourRepository extends CrudRepository<Tour, Integer> {
+//public interface TourRepository extends PagingAndSortingRepository<Tour, Integer> {
 
     /**
      * Find Tours associated with the Tour Package
@@ -48,7 +48,31 @@ public interface TourRepository extends PagingAndSortingRepository<Tour, Integer
 
     List<Tour> findByRegionIn(List<Region> regions);
 
-    Tour save(Tour tour);
+  //  Tour save(Tour tour);
 
-    Integer count();
+    long count();
+
+    @Override
+    @RestResource(exported = false)
+    <T extends Tour> T save(T t);
+
+    @Override
+    @RestResource(exported = false)
+    <T extends Tour> Iterable<T> saveAll(Iterable<T> iterable);
+
+    @Override
+    @RestResource(exported = false)
+    void deleteById(Integer id);
+
+    @Override
+    @RestResource(exported = false)
+    void delete(Tour tour);
+
+    @Override
+    @RestResource(exported = false)
+    void deleteAll(Iterable<? extends Tour> iterable);
+
+    @Override
+    @RestResource(exported = false)
+    void deleteAll();
 }
